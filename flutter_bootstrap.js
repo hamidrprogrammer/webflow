ce7ee755341290d3ca2a1c72f47cb9bf7d6e5506
HamidReza <hamidrdeveloper@gmail.com>
2025-05-07 11:49:39 +0300

    first commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3489058958"
+    serviceWorkerVersion: "2854030637"
   }
 });
